d474dec4180eeff02e802cffdab555055acf7d17
Vaishali2127 <vaishalimahour8@gmail.com>
2022-03-28 22:45:05 +0530

    changes made in news

---
+++ src/services/cryptoNewsApi.js
@@ -0,0 +1,28 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+const cryptoNewsHeaders = {
+  headers: {
+    "X-BingApis-SDK": "true",
+    "X-RapidAPI-Host": "bing-news-search1.p.rapidapi.com",
+    "X-RapidAPI-Key": "3345b51ef2msh578be087f685ad6p1f68b2jsn0ede18228e3d",
+  },
+};
+
+const baseUrl = "https://bing-news-search1.p.rapidapi.com";
+
+const createRequest = (url) => ({ url, headers: cryptoNewsHeaders });
+
+export const cryptoNewsApi = createApi({
+  reducerPath: "cryptoNewsApi",
+  baseQuery: fetchBaseQuery({ baseUrl }),
+  endpoints: (builder) => ({
+    getCryptoNews: builder.query({
+      query: ({ newsCategory, count }) =>
+        createRequest(
+          `/news/search?q=${newsCategory}&safeSearch=Off&textFormat=Raw&freshness=Day&count=${count}`
+        ),
+    }),
+  }),
+});
+
+export const { useGetCryptoNewsQuery } = cryptoNewsApi;
