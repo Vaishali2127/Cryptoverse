1de81dda58cee076561cacd42f08288847309546
Vaishali2127 <vaishalimahour8@gmail.com>
2022-04-02 13:07:53 +0530

    monor changes done

--- src/components/Exchange.jsx
+++ src/components/Exchange.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import { Row, Col } from "antd";
+
+import { useGetExchangesQuery } from "../services/cryptoApi";
+import Loader from "./Loader";
 
 const Exchange = () => {
-  return <div>Exchange</div>;
+  return (
+    <>
+      <Row>
+        <Col span={6}>Exchanges</Col>
+        <Col span={6}>24h Trade Volume</Col>
+        <Col span={6}>Markets</Col>
+        <Col span={6}>Change</Col>
+      </Row>
+    </>
+  );
 };
 
 export default Exchange;
